f404ebf3bb4ddb1efe7cbd4afbda90860b788383
HemangSharma06 <146320981+HemangSharma06@users.noreply.github.com>
2026-07-23 22:38:06 +0530

    Added Streamlit UI wih FastAPI

--- backend/ingestion/VectorDB.py
+++ backend/ingestion/VectorDB.py
@@ -1,9 +1,20 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 # from langchain_openai import OpenAIEmbeddings
 from langchain_qdrant import QdrantVectorStore
+from qdrant_client.models import VectorParams, Distance
 
 def createVectorStore(chunks, client):
+    from qdrant_client.http.exceptions import UnexpectedResponse
 
+    if not client.collection_exists("omnibrain"):
+        client.create_collection(
+            collection_name="omnibrain",
+            vectors_config=VectorParams(
+                size=384,
+                distance=Distance.COSINE
+            )
+        )
+        
     embedding_model = HuggingFaceEmbeddings(
         model_name="BAAI/bge-small-en-v1.5"
     )
