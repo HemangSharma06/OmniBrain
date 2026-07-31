f9d008db788eb066176f2861cbfdfe9ca5737735
HemangSharma06 <146320981+HemangSharma06@users.noreply.github.com>
2026-08-01 00:01:06 +0530

    Edited quadrant database

--- backend/ingestion/VectorDB.py
+++ backend/ingestion/VectorDB.py
@@ -1,10 +1,11 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 # from langchain_openai import OpenAIEmbeddings
+
 from langchain_qdrant import QdrantVectorStore
-from qdrant_client.models import VectorParams, Distance
+from qdrant_client.models import VectorParams, Distance, PointStruct
+import uuid
 
 def createVectorStore(chunks, client):
-    from qdrant_client.http.exceptions import UnexpectedResponse
 
     if not client.collection_exists("omnibrain"):
         client.create_collection(
@@ -14,20 +15,53 @@ def createVectorStore(chunks, client):
                 distance=Distance.COSINE
             )
         )
-        
+
+    # Embedding Model
     embedding_model = HuggingFaceEmbeddings(
         model_name="BAAI/bge-small-en-v1.5"
     )
-    # Embedding Model
+
     # embedding_model = OpenAIEmbeddings(
     #     model="text-embedding-3-small"
     # )
-    
+
     vectorstore = QdrantVectorStore(
         embedding=embedding_model,
         client=client,
         collection_name="omnibrain"
     )
+
     vectorstore.add_documents(chunks)
+
     return vectorstore
-    
+
+
+
+def createImageVectorStore(image_vectors, client):
+
+    collection = "omnibrain_images"
+    if not client.collection_exists(collection):
+        client.create_collection(
+            collection_name=collection,
+            vectors_config=VectorParams(
+                size=512,
+                distance=Distance.COSINE
+            )
+        )
+
+    points = [
+        PointStruct(
+            id=str(uuid.uuid4()),
+            vector=image["embedding"],
+            payload={
+                "path": image["path"],
+                "type": "image"
+            }
+        )
+        for image in image_vectors
+    ]
+    client.upsert(
+        collection_name=collection,
+        points=points
+    )
+    return True
