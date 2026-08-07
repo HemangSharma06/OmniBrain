7aae8882c27b1692b8f0b1d768b3d9a142732728
HemangSharma06 <146320981+HemangSharma06@users.noreply.github.com>
2026-08-07 19:57:46 +0530

    Changes into the Vision Agent for newly added state

--- backend/agents/visionAgent.py
+++ backend/agents/visionAgent.py
@@ -13,12 +13,9 @@ def vision_agent(state: dict) -> dict:
         raise ValueError("No image_paths found in state.")
 
     analyses = []
-
     for image_path in image_paths:
-
         try:
             image = Image.open(image_path)
-
             message = HumanMessage(
                 content=[
                     {
@@ -33,41 +30,31 @@ def vision_agent(state: dict) -> dict:
             )
 
             response = vision_llm.invoke([message])
-
             if hasattr(response, "content"):
-
                 if isinstance(response.content, list):
-
                     text = []
-
                     for block in response.content:
-
                         if (
                             isinstance(block, dict)
                             and block.get("type") == "text"
                         ):
                             text.append(block["text"])
-
                     result = "\n".join(text).strip()
-
                 else:
                     result = str(response.content).strip()
-
             else:
                 result = str(response)
-
             analyses.append(
                 f"Image: {image_path}\n{result}"
             )
-
         except Exception as e:
             analyses.append(
                 f"Image: {image_path}\nError: {str(e)}"
             )
 
     print(f"[Vision Agent]: Analyzed {len(image_paths)} image(s).")
-    
     print(f"\n|---- Time Taken = {time.time()-start : .2f} ----|")
+    
     return {
         "vision_context": analyses
     }
