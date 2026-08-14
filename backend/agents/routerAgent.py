61b6218669c3ccae6a51ee024bd4cb4abec32bdf
HemangSharma06 <146320981+HemangSharma06@users.noreply.github.com>
2026-08-14 21:17:29 +0530

    Fix the router agent

--- backend/agents/routerAgent.py
+++ backend/agents/routerAgent.py
@@ -1,8 +1,7 @@
 from backend.llm.llm import getRouterDecision
 from backend.llm.prompt import router_prompt
-from backend.ingestion.tableIngestion import engine
-from sqlalchemy import inspect
 import time
+
 def router_agent(state: dict) -> dict:
     start = time.time()
     print("\n[Router Agent]: Analyzing user intent...")
@@ -17,10 +16,8 @@ def router_agent(state: dict) -> dict:
     # Safety fallback
     if next_agent not in ["SEARCH", "VISION", "SQL"]:
         next_agent = "SEARCH"
-
     print(f"\n[Router Agent]: Routing execution to -> {next_agent}")
     print(f"\n|---- Time Taken = {time.time()-start : .2f} ----|")
-    
     return {
         "next_step": next_agent
     }
